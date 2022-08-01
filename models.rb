e8f6831993ea008935da48a041e591cdeca8180b
Jeremy Evans <code@jeremyevans.net>
2022-08-01 15:57:14 -0700

    Use require_valid_schema Sequel plugin

--- models.rb
+++ models.rb
@@ -18,6 +18,7 @@ Model.plugin :subclasses
 Model.plugin :prepared_statements
 Model.plugin :pg_auto_constraint_validations
 Model.plugin :auto_restrict_eager_graph
+Model.plugin :require_valid_schema
 if ENV['RACK_ENV'] == 'test'
   Model.plugin :forbid_lazy_load
   Model.plugin :instance_specific_default, :warn
